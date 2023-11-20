29a008f7706249f4934c5ad94e8da6ea47944ec7
apoto <apopo.apoto930@gmail.com>
2023-11-20 23:29:11 +0900

    マイページのUI構築

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -9,6 +9,171 @@ class MyPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('マイページ'),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Image.network(
+                    'https://i.pinimg.com/474x/f9/5c/55/f95c5525bea42df3611512e92a190c6a.jpg',
+                    width: 60,
+                    height: 60,
+                  ),
+                  const Spacer(),
+                  const Column(
+                    children: [
+                      Text(
+                        '7,041',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('投稿'),
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                  const Column(
+                    children: [
+                      Text(
+                        '4.6億',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('フォロワー'),
+                    ],
+                  ),
+                  const SizedBox(width: 16),
+                  const Column(
+                    children: [
+                      Text(
+                        '99',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text('フォロー'),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Instagram',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const Text(
+                '#YoursToMake',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+              const Text(
+                'help.instagram.com',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            ),
+                            child: const Text('フォロー中'),
+                          ),
+                        ),
+                        const SizedBox(width: 4),
+                        Expanded(
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            style: OutlinedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            ),
+                            child: const Text(
+                              'メッセージ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    child: const Icon(Icons.keyboard_arrow_down),
+                  ),
+                ],
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                primary: false,
+                crossAxisCount: 3,
+                children: const <Widget>[
+                  InstagramPostItem(
+                    imageUrl:
+                        'https://livedoor.blogimg.jp/cordial8317-gourmet/imgs/6/9/69509807.jpg',
+                  ),
+                  InstagramPostItem(
+                    imageUrl:
+                        'https://www.sapporo.travel/sp/sapporo-ramen/images/mainv.jpg',
+                  ),
+                  InstagramPostItem(
+                    imageUrl:
+                        'https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/01/a0001697/img/basic/a0001697_main.jpg',
+                  ),
+                  InstagramPostItem(
+                    imageUrl:
+                        'https://mall.premium-water.net/img/goods/L/KWK-007_0d46f33720c843c18b67bd9cd4922e39.jpg',
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class InstagramPostItem extends StatelessWidget {
+  const InstagramPostItem({super.key, required this.imageUrl});
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      imageUrl,
+      fit: BoxFit.cover,
     );
   }
 }
